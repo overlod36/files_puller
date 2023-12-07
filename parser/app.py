2c6037a62a9f9a9343ffbbd6f784edbb61e8e2e7
kirbi <Biehan@mail.ru>
2023-12-07 17:23:52 +0300

    bruh

--- parser/app.py
+++ parser/app.py
@@ -1,9 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-from bs4 import BeautifulSoup
 from datetime import datetime
 import json
 
