a2bd2d1089769e6fe3785582b88a3a12c0ea7726
kirbi <Biehan@mail.ru>
2023-12-07 15:30:21 +0300

    get all articles for every journals, save results as json file, parser is ready

--- parser/app.py
+++ parser/app.py
@@ -4,7 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
-import time
+from datetime import datetime
+import json
 
 URL = 'https://dl.acm.org/loi/cacm/group/'
 
@@ -17,25 +18,49 @@ driver.implicitly_wait(2)
 dec_panel = driver.find_element(By.CLASS_NAME, 'scroll')
 dec_elems = dec_panel.find_elements(By.TAG_NAME, 'li')
 
+# берем все пары decade.year
 for du in driver.find_elements(By.CLASS_NAME, 'scroll')[1:len(dec_elems)+1]:
     for year in du.find_elements(By.TAG_NAME, 'li'):
         years_data_urls.append(year.find_element(By.TAG_NAME, "a").get_attribute("data-url"))
 tab_content_counter = 1
 
+# и идем по каждому году
 for data_url in years_data_urls:
-    current_year_journal = []
+    current_year_journals = []
     driver.get(f'{URL}{data_url}')
 
+    # убираем окно с куки
     if len(driver.find_elements(By.ID, 'CybotCookiebotDialogBody')) != 0:
         WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.ID, 'CybotCookiebotDialogBodyLevelButtonLevelOptinDeclineAll'))).click()
     
+    # ищем список журналов текущего года
     while len(driver.find_elements(By.CLASS_NAME, 'tab__content')[tab_content_counter].find_elements(By.TAG_NAME, 'li')) == 0:
         tab_content_counter += 1
     content = driver.find_elements(By.CLASS_NAME, 'tab__content')[tab_content_counter].find_elements(By.TAG_NAME, 'li')
     
+    # заполняем предварительный список журналов текущего года
     for journal in content:
-        current_year_journal.append({'href': journal.find_element(By.TAG_NAME, 'a').get_attribute('href'),
+        current_year_journals.append({'href': journal.find_element(By.TAG_NAME, 'a').get_attribute('href'),
                                      'title': f'{journal.find_element(By.CLASS_NAME, "coverDate").text}, {journal.find_element(By.CLASS_NAME, "issue").text}'})
-    journals[data_url] = current_year_journal
+    
+    # заходим на каждую страницу журнала, записываем ссылки на все доступные статьи
+    for sp_journal in current_year_journals:
+        driver.get(sp_journal['href'])
+        journals_elems = driver.find_elements(By.CLASS_NAME, 'issue-item-container')
+        articles = []
+        for j_el in journals_elems:
+            file_name = j_el.find_element(By.CLASS_NAME, 'issue-item__content-right').find_element(By.TAG_NAME, 'a').text
+            file_details = j_el.find_element(By.CLASS_NAME, 'issue-item__detail').find_element(By.TAG_NAME, 'a').text
+            articles.append({file_name: file_details})
+        sp_journal['articles'] = articles
+    
+    # сохраняем статьи и прикрепляем их к журналу
+    journals[data_url] = current_year_journals
+
+result = json.dumps(journals, indent=4)
+
+# запись в файл
+with open(f'results/{datetime.now().strftime("%H%M%S%f")}.json', 'w') as file:
+    file.write(result)
 
 driver.close()
